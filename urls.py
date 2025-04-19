f86d65e1090ba6b3f4e6afe75bda6352045616e7
Prakash-RajS <senthilprakash55@gmail.com>
2025-04-19 15:13:57 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,57 +1,46 @@
-"""
-URL configuration for my_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-"""from django.contrib import admin
-from django.urls import path, include
+"""from django.urls import path
+from . import views  # to use views from views.py
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('appln.urls')) #used to connect url with appln/urls.py
+    path('', views.login_page, name='login'),  # Home page shows login.html
+    path('signup/', views.signup_view, name='signup'),  # Go to signup page
+    path('index/', views.index_view, name='index'),  # Go to index page (Home page after login)
+    path('login/', views.login_page, name='login_view'),  # Explicit login view for form submission
 ]
 """
-"""#Update this for wokinf with react.js
+
+# urls.py
+"""from django.urls import path
+from .views import FrontendAppView
+
+urlpatterns = [
+    path('', FrontendAppView.as_view(), name='frontend'),
+    # other urls...
+]
+"""
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,  include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),  # React index.html
-]"""
-"""from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Catch-all route for React - must be last
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
     path('', TemplateView.as_view(template_name='index.html')),
-]
+    #path("login/<str:provider>/", views.auth_login, name="auth_login"),
+    #path("callback/", views.auth_callback, name="auth_callback"),
+     # Social login routes
+    path('google/', views.auth_login, {'provider': 'google-oauth2'}, name='google_login'),
+    path('facebook/', views.auth_login, {'provider': 'facebook'}, name='facebook_login'),
+    path('apple/', views.auth_login, {'provider': 'apple'}, name='apple_login'),
 
-"""
+    # Callback URL from Auth0
+    path('callback/', views.auth_callback, name='auth_callback'),
+    path('auth/', include('social_django.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-from django.contrib import admin
-from django.urls import path, include
-from appln import views 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('appln.urls')), #for auth0
-    path('auth/', include('social_django.urls')),  # For social authentication
-    path('signup/', include('appln.urls')),
-    path('', views.signup_view, name='home'),  # Add this line for the root URL
-]
 #check "auth0 update" comment
