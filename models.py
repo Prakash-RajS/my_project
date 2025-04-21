ab318478218532d3df2d649271d87715eb366ed5
Prakash-RajS <senthilprakash55@gmail.com>
2025-04-21 09:24:04 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class UserData(models.Model):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=255, null=True, blank=True)
     provider = models.CharField(max_length=50, null=True, blank=True)  # 'google', 'facebook', 'apple' etc.
 
     def __str__(self):
