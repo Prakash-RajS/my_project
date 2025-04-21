2c73a515801369b260bbe74a5d921e30bf4e7998
Prakash-RajS <senthilprakash55@gmail.com>
2025-04-21 18:50:58 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class UserData(models.Model):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255, null=True, blank=True)
-    provider = models.CharField(max_length=50, null=True, blank=True)  # 'google', 'facebook', 'apple' etc.
-
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    provider = models.CharField(max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     def __str__(self):
         return self.email
