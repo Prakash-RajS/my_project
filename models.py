2e586f63357c37d4e167c7aed2350f8931afb958
Prakash-RajS <senthilprakash55@gmail.com>
2025-05-05 11:51:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -45,16 +45,15 @@ class UserSubscription(models.Model):
     user = models.ForeignKey('UserData', on_delete=models.CASCADE, related_name='subscriptions')
     email = models.EmailField(blank=True, null=True)
     userid = models.CharField(max_length=255, blank=True, null=True)
-    first_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
     current_plan = models.CharField(max_length=10, choices=PLAN_CHOICES, default='basic')
     pricing = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
 
     start_date = models.DateField(auto_now_add=True)
-    plan_expiring_date = models.DateTimeField()
-    renews_on = models.DateField()
+    plan_expiring_date = models.DateTimeField(blank=True, null=True)
+    renews_on = models.DateField(blank=True, null=True)
 
-    total_credits = models.IntegerField(default=10)  # Based on plan
+    total_credits = models.IntegerField(default=10)
     used_credits = models.IntegerField(default=0)
     total_credits_used_all_time = models.IntegerField(default=0)
 
@@ -65,8 +64,8 @@ class UserSubscription(models.Model):
     def save(self, *args, **kwargs):
         if not self.renews_on:
             self.renews_on = timezone.now().date() + relativedelta(months=1)
-        if not self.plan_expiring_date:  # Ensure plan_expiring_date is set if not already
-            self.plan_expiring_date = timezone.now() + relativedelta(months=1)  # Set expiry to 1 month from now
+        if not self.plan_expiring_date:
+            self.plan_expiring_date = timezone.now() + relativedelta(months=1)
         super().save(*args, **kwargs)
 
     def __str__(self):
