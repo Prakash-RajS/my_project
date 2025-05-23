3699231e6368416f38160ebf66d7905fe5e1ccf8
Prakash-RajS <senthilprakash55@gmail.com>
2025-05-23 12:04:46 +0530

    Add files via upload

---
+++ ContactPg/HeroContact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactBanner from './ContactBanner'
+import ContactForm from './ContactForm'
+
+export default function HeroContact() {
+  return (
+    <div>
+      
+      <ContactBanner/>
+
+      <ContactForm/>
+    </div>
+  )
+}
